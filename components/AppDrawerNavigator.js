08f832b2667c69dfcfa6e66677368d52a2bc558f
vedantsanghi <64643589+vedantsanghi@users.noreply.github.com>
2021-01-17 13:22:30 +0530

    Add files via upload

---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { AppTabNavigator } from "./AppTabNavigator";
+import CustomSideBarMenu from "./CustomSideBarMenu";
+import SettingScreen from "../screens/SettingScreen";
+import MyDonationScreen from "../screens/MyDonationScreen";
+
+export const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: AppTabNavigator,
+    },
+    MyBarters: {
+      screen: MyDonationScreen,
+    },
+
+    Setting: {
+      screen: SettingScreen,
+    },
+  },
+  {
+    contentComponent: CustomSideBarMenu,
+  },
+  {
+    initialRouteName: "Home",
+  }
+);
